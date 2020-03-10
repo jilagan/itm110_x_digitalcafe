4051838ebb5cda9f382c7c32c1e65ca5d4af4b0c
Joseph Ilagan <joben.ilagan@gmail.com>
2020-03-10 16:59:08 +0800

    New repo

---
+++ app.py
@@ -0,0 +1,74 @@
+from flask import Flask,redirect
+from flask import render_template
+from flask import request
+from flask import session
+import database as db
+import authentication
+import logging
+
+app = Flask(__name__)
+
+# Set the secret key to some random bytes.
+# Keep this really secret!
+app.secret_key = b's@g@d@c0ff33!'
+
+
+logging.basicConfig(level=logging.DEBUG)
+app.logger.setLevel(logging.INFO)
+
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', page="Index")
+
+@app.route('/products')
+def products():
+    product_list = db.get_products()
+    return render_template('products.html', page="Products", product_list=product_list)
+
+@app.route('/productdetails')
+def productdetails():
+    code = request.args.get('code', '')
+    product = db.get_product(int(code))
+
+    return render_template('productdetails.html', code=code, product=product)
+
+@app.route('/branches')
+def branches():
+    branch_list = db.get_branches()
+    return render_template('branches.html', page="Branches", branch_list=branch_list)
+
+@app.route('/branchdetails')
+def branchdetails():
+    code = request.args.get('code', '')
+    product = db.get_branch(int(code))
+
+    return render_template('productdetails.html', code=code, product=product)
+
+@app.route('/aboutus')
+def aboutus():
+    return render_template('aboutus.html', page="About Us")
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
+
+@app.route('/auth', methods = ['GET', 'POST'])
+def auth():
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    is_successful, user = authentication.login(username, password)
+    app.logger.info('%s', is_successful)
+    if(is_successful):
+        session["user"] = user
+        return redirect('/')
+    else:
+        return redirect('/login')
+
+@app.route('/logout')
+def logout():
+    session.pop("user",None)
+    return redirect('/')
